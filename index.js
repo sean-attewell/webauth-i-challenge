e1b4991b2a520beca9be54c60f7e879ee9465a18
sean-attewell <sean.attewell@gmail.com>
2019-04-01 14:43:24 +0100

    Day 1 MVP done (2 day project)

--- index.js
+++ index.js
@@ -15,6 +15,44 @@ server.get('/', (req, res) => {
     res.send("It's alive!");
 });
 
+// Register a new user
+// Hash password before saving it to the database
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+    let hashedPw = bcrypt.hashSync(user.password, 10);  // 2 ^ 10
+
+    user.password = hashedPw;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+
+// User login 
+// bycrypt compares the hash of the login pass 
+// to the hash held on file for that user 
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
 
 
 
